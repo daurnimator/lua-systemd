a1dd32c5b574edf2ba01a51dd887bc097d2724db
daurnimator <quae@daurnimator.com>
2014-08-08 11:35:02 -0400

    Squashed 'vendor/compat-5.2/' changes from 0322560..898ede6
    git-subtree-dir: vendor/compat-5.2
    git-subtree-split: 898ede60ff63ffffb9a2116a151553b2c0f489b0

--- c-api/compat-5.2.h
+++ c-api/compat-5.2.h
@@ -1,5 +1,8 @@
+#include <stddef.h>
+#include <stdio.h>
 #include "lua.h"
 #include "lauxlib.h"
+#include "lualib.h"
 
 #if !defined(LUA_VERSION_NUM)
 /* Lua 5.0 */
@@ -42,6 +45,11 @@ const char *luaL_tolstring (lua_State *L, int idx, size_t *len);
 
 #if !defined(LUA_VERSION_NUM) || LUA_VERSION_NUM == 501
 /* Lua 5.0 *or* 5.1 */
+
+typedef struct luaL_Stream {
+  FILE *f;
+  lua_CFunction closef;
+} luaL_Stream;
 
 #define lua_pushglobaltable(L) \
   lua_pushvalue(L, LUA_GLOBALSINDEX)
