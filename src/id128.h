eda6adb0f36a012990cc7a069a8889da5a365a6f
daurnimator <quae@daurnimator.com>
2014-10-06 00:34:37 -0400

    src/*.h: Remember to include lua.h for lua_State definition

--- src/id128.h
+++ src/id128.h
@@ -1,3 +1,5 @@
+#include "lua.h"
+
 #define ID128_METATABLE "ID128"
 
 int luaopen_systemd_id128_core (lua_State *L);
--- src/journal.h
+++ src/journal.h
@@ -1,3 +1,5 @@
+#include "lua.h"
+
 #define JOURNAL_METATABLE "sd_journal"
 
 int luaopen_systemd_journal_core (lua_State *L);
