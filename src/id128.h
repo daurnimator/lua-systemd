86aa8b0768313cdad052fa7334f19d626cd73cec
daurnimator <quae@daurnimator.com>
2014-10-12 16:31:24 -0400

    src/id128.h: Fix incorrect condition in check_id128_t

--- src/id128.h
+++ src/id128.h
@@ -9,7 +9,7 @@ static sd_id128_t check_id128_t (lua_State *L, int arg) {
 		case LUA_TUSERDATA:
 			return *(sd_id128_t*)luaL_checkudata(L, arg, ID128_METATABLE);
 		case LUA_TSTRING:
-			luaL_argcheck(L, sd_id128_from_string(lua_tostring(L, arg), &boot_id) != 0, arg, "string is not a valid id128");
+			luaL_argcheck(L, sd_id128_from_string(lua_tostring(L, arg), &boot_id) == 0, arg, "string is not a valid id128");
 			return boot_id;
 		default:
 			luaL_argerror(L, arg, lua_pushfstring(L, "id128 expected, got %s", luaL_typename(L, arg)));
