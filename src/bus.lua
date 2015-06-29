03e701b1a13bdd28f8b0b79dd051caa01340ad62
daurnimator <quae@daurnimator.com>
2015-06-29 14:10:57 +1000

    src/bus.lua: Nicer __tostring for bus errors

--- src/bus.lua
+++ src/bus.lua
@@ -1,3 +1,10 @@
 local c = require "systemd.bus.core"
 
+function c.BUS_ERROR_METATABLE:__tostring()
+	return string.format("sd_bus_error: %s: %s",
+		self:get_name(),
+		self:get_message()
+	)
+end
+
 return c
